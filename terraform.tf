33ccfa7836078ebdf51e86ea17e3dad7539d1b4f
Sergio Cogollos Del Campo <scogollo@emeal.nttdata.com>
2022-11-09 16:53:42 +0100

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,12 @@
 terraform {
 
-  cloud {
-    organization = "organization-name"
+  /* cloud {
+    organization = "nttdata-test"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
-  }
+  } */
 
   required_providers {
     aws = {
